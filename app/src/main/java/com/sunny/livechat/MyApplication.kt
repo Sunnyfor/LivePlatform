ec6a106c2109f8a0ab086705a22acdd1a0de4fc3
chenyongzuo <yongzuo.chen@foxmail.com>
2019-12-30 17:23:45 +0800

    添加持久化保存数据的方法

--- app/src/main/java/com/sunny/livechat/MyApplication.kt
+++ app/src/main/java/com/sunny/livechat/MyApplication.kt
@@ -1,13 +1,14 @@
 package com.sunny.livechat
 
 import android.app.Application
-import com.orhanobut.logger.*
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
 import com.sunny.livechat.chat.MLOC
 import com.sunny.livechat.util.NetworkUtil
 
 
 /**
- * Desc
+ * Desc 全局管理类
  * Author JoannChen
  * Mail yongzuo.chen@foxmail.com
  * Date 2019/10/22 13:28
@@ -15,12 +16,20 @@ import com.sunny.livechat.util.NetworkUtil
 class MyApplication : Application() {
 
     /**
+     * 内存数据存储
+     */
+    private val saveDataMap = HashMap<String, Any>()
+
+
+    /**
      * 单例
      */
     companion object {
+
         private lateinit var instance: MyApplication
-        fun getInstance(): MyApplication =
-            instance
+
+        fun getInstance(): MyApplication = instance
+
     }
 
     override fun onCreate() {
@@ -34,4 +43,43 @@ class MyApplication : Application() {
         Logger.addLogAdapter(AndroidLogAdapter())
 
     }
+
+    /**
+     * 取数据
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun <T> getData(key: String, isDelete: Boolean): T? {
+
+        if (!saveDataMap.containsKey(key)) {
+            return null
+        }
+
+        val result = saveDataMap[key]
+
+        if (isDelete) {
+            removeData(key)
+        }
+        return result as T
+    }
+
+    fun <T> getData(key: String): T? {
+        return getData(key, false)
+    }
+
+    /**
+     * 存储数据
+     */
+    fun putData(key: String, t: Any?) {
+        if (t != null) {
+            saveDataMap[key] = t
+        }
+    }
+
+    /**
+     * 删除数据
+     */
+    fun removeData(key: String) {
+        saveDataMap.remove(key)
+    }
+
 }
